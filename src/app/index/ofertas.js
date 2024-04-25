563fdb9a1bab967ef23913d1cea2ed64dfd390ba
Lazarsilke <magdielespinoza8@gmail.com>
2024-04-25 00:08:40 -0600

    LoginForms
    Agregué los formularios para login y registro y hice unos pequeños cambios, luego agrego el header y el menu desplegable en ambos para terminar creando la base de datos

---
+++ src/app/index/ofertas.js
@@ -0,0 +1,36 @@
+// JavaScript para la galería de ofertas
+let currentOffer = 0;
+const offerImages = [
+    "/img/mango.jpg",
+    "/img/mango2.jpg",
+    "/img/mango3.jpg"
+]; // Ruta de las imágenes de oferta
+
+function showOffer() {
+    const offerSlide = document.querySelector(".offer-slide");
+    offerSlide.innerHTML = `<img src="${offerImages[currentOffer]}" alt="Oferta ${currentOffer + 1}">`;
+}
+
+function nextOffer() {
+    currentOffer++;
+    if (currentOffer >= offerImages.length) {
+        currentOffer = 0;
+    }
+    showOffer();
+}
+
+function prevOffer() {
+    currentOffer--;
+    if (currentOffer < 0) {
+        currentOffer = offerImages.length - 1;
+    }
+    showOffer();
+}
+
+// Cambiar automáticamente las imágenes cada 3 segundos
+setInterval(nextOffer, 3000);
+
+// Mostrar la primera imagen al cargar la página
+showOffer();
+
+
---
+++ src/app/logeo/logeo.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-logeo',
+  standalone: true,
+  imports: [],
+  templateUrl: './logeo.component.html',
+  styleUrl: './logeo.component.css'
+})
+export class LogeoComponent {
+
+}
